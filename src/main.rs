53f0d5efb732c624ccb520da60c5a06d94f5c3e7
Linus <linus@bravomedia.se>
2024-06-18 21:21:23 +0200

    Removed hard coded number of threads

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rusty_barn::{config, http::{self}};
 
-#[tokio::main(flavor = "multi_thread", worker_threads = 16)]
+#[tokio::main(flavor = "multi_thread")]
 async fn main() {
     http::serve(config::Config::new(), None).await;
 }
